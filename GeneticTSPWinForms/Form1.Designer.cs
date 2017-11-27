947975b4249075f2b33f03df33aa1d942654371b
Wojciech Regulski <wojtekreg@gmail.com>
2017-11-27 21:00:08 +0100

    some initial things, now it's loading instance and displaying greedy tour

--- GeneticTSPWinForms/Form1.Designer.cs
+++ GeneticTSPWinForms/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tourDiagram = new System.Windows.Forms.PictureBox();
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.filenameLabel = new System.Windows.Forms.Label();
+            this.loadInstanceButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tourDiagram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tourDiagram
+            // 
+            this.tourDiagram.BackColor = System.Drawing.Color.White;
+            this.tourDiagram.Location = new System.Drawing.Point(12, 12);
+            this.tourDiagram.Name = "tourDiagram";
+            this.tourDiagram.Size = new System.Drawing.Size(500, 500);
+            this.tourDiagram.TabIndex = 0;
+            this.tourDiagram.TabStop = false;
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.Location = new System.Drawing.Point(568, 315);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.filenameTextBox.TabIndex = 1;
+            this.filenameTextBox.Text = "D:\\tsp.txt";
+            // 
+            // filenameLabel
+            // 
+            this.filenameLabel.AutoSize = true;
+            this.filenameLabel.Location = new System.Drawing.Point(565, 299);
+            this.filenameLabel.Name = "filenameLabel";
+            this.filenameLabel.Size = new System.Drawing.Size(49, 13);
+            this.filenameLabel.TabIndex = 2;
+            this.filenameLabel.Text = "Filename";
+            // 
+            // loadInstanceButton
+            // 
+            this.loadInstanceButton.Location = new System.Drawing.Point(568, 341);
+            this.loadInstanceButton.Name = "loadInstanceButton";
+            this.loadInstanceButton.Size = new System.Drawing.Size(100, 23);
+            this.loadInstanceButton.TabIndex = 3;
+            this.loadInstanceButton.Text = "Load instance";
+            this.loadInstanceButton.UseVisualStyleBackColor = true;
+            this.loadInstanceButton.Click += new System.EventHandler(this.loadInstanceButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(568, 370);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 533);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.loadInstanceButton);
+            this.Controls.Add(this.filenameLabel);
+            this.Controls.Add(this.filenameTextBox);
+            this.Controls.Add(this.tourDiagram);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tourDiagram)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox tourDiagram;
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.Label filenameLabel;
+        private System.Windows.Forms.Button loadInstanceButton;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- GeneticTSPWinForms/Form1.cs
+++ GeneticTSPWinForms/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,178 @@ namespace GeneticTSPWinForms
 {
     public partial class Form1 : Form
     {
+        private StreamReader file;
+        private int numberOfNodes;
+        private int[,] nodes;
+        private int ratio;
+        private List<int> globalRoute;
+        private double globalRouteLength;
+        private CancellationTokenSource tokenSource;
+        private CancellationToken token;
         public Form1()
         {
             InitializeComponent();
+            globalRouteLength = Double.MaxValue;
+            globalRoute = new List<int>();
+        }
+
+        private void loadInstanceButton_Click(object sender, EventArgs e)
+        {
+            file = new StreamReader(filenameTextBox.Text);
+            numberOfNodes = int.Parse(file.ReadLine());
+            nodes = new int[numberOfNodes, 2];
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                string temp = file.ReadLine();
+                nodes[i, 0] = int.Parse(temp.Split(' ')[1]);
+                nodes[i, 1] = int.Parse(temp.Split(' ')[2]);
+            }
+            file.Close();
+            int x = Int32.MinValue;
+            int y = Int32.MinValue;
+            ratio = 1;
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                if (nodes[i,0]>x)
+                {
+                    x = nodes[i, 0];
+                }
+                if (nodes[i, 1] > y)
+                {
+                    y = nodes[i, 1];
+                }
+            }
+            //filenameTextBox.Text = x + " " + y;
+            if (x>y)
+            {
+                ratio = x / 500;
+                if (x % 500 != 0)
+                {
+                    ratio++;
+                }
+            }
+            else
+            {
+                ratio = y / 500;
+                if (y % 500 != 0)
+                {
+                    ratio++;
+                }
+            }
+            Image cityImage = new Bitmap(tourDiagram.Width, tourDiagram.Height);
+            Graphics graphics = Graphics.FromImage(cityImage);
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                graphics.DrawEllipse(Pens.Black, nodes[i, 0] / ratio - 2, nodes[i, 1] / ratio - 2, 4, 4);
+            }
+            tourDiagram.Image = cityImage;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (startButton.Text.Equals("Start"))
+            {
+                startButton.Text = "Stop";
+                tokenSource = new CancellationTokenSource();
+                token = tokenSource.Token;
+
+                Task tMain = Task.Run(() =>
+                {
+                    globalRoute.Clear();
+                    globalRouteLength = Double.MaxValue;
+                    /*for (int i = 0; i < 29; i++)
+                    {
+                        globalRoute.Add(i);
+                    }
+                    globalRoute.Add(0);*/
+                    for (int k = 0; k < numberOfNodes; k++)
+                    {
+                        if (token.IsCancellationRequested)
+                            break;
+                        List<int> route = new List<int>();
+                        List<int> notUsedNodes = new List<int>();
+                        double routeLength = 0;
+                        for (int i = 0; i < numberOfNodes; i++)
+                        {
+                            notUsedNodes.Add(i);
+                        }
+
+                        int firstNode = k;
+                        route.Add(firstNode);
+                        notUsedNodes.Remove(firstNode);
+                        while (notUsedNodes.Count > 0)
+                        {
+                            int actualNodeNumber = route[route.Count - 1];
+                            int actualNodeX = nodes[actualNodeNumber, 0];
+                            int actualNodeY = nodes[actualNodeNumber, 1];
+                            double minLength = Double.MaxValue;
+                            int chosenNode = -1;
+
+                            foreach (int nodeNumber in notUsedNodes)
+                            {
+                                int x = nodes[nodeNumber, 0];
+                                int y = nodes[nodeNumber, 1];
+                                double length = Math.Sqrt(Math.Pow(actualNodeX - x, 2) + Math.Pow(actualNodeY - y, 2));
+                                if (length < minLength)
+                                {
+                                    minLength = length;
+                                    chosenNode = nodeNumber;
+                                }
+                            }
+
+                            routeLength += minLength;
+                            route.Add(chosenNode);
+                            notUsedNodes.Remove(chosenNode);
+                        }
+
+                        route.Add(route[0]);
+                        routeLength += Math.Sqrt(Math.Pow(nodes[route[0], 0] - nodes[route[route.Count - 1], 0], 2) +
+                                                 Math.Pow(nodes[route[0], 1] - nodes[route[route.Count - 1], 1], 2));
+
+                        if (routeLength < globalRouteLength)
+                        {
+                            globalRouteLength = routeLength;
+                            globalRoute.Clear();
+                            for (int i = 0; i < route.Count; i++)
+                            {
+                                globalRoute.Add(route[i]);
+                            }
+                            Task tDr = Task.Run(() =>
+                            {
+                                DrawTour();
+                            });
+                        }
+                    }
+                    startButton.BeginInvoke(new MethodInvoker(() =>
+                    {
+                        startButton.Text = "Start";
+                    }));
+                }, token);
+            }
+            else
+            {
+                tokenSource.Cancel();
+                //startButton.Text = "Start";
+            }
+            //tMain.Wait();
+            //DrawTour();
+            //filenameTextBox.Text = globalRoute.Count.ToString();
+        }
+
+        private void DrawTour()
+        {
+            Image cityImage = new Bitmap(tourDiagram.Width, tourDiagram.Height);
+            Graphics graphics = Graphics.FromImage(cityImage);
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                graphics.DrawEllipse(Pens.Black, nodes[i, 0] / ratio - 2, nodes[i, 1] / ratio - 2, 4, 4);
+            }
+            for (int i = 0; i < globalRoute.Count - 1; i++)
+            {
+                graphics.DrawLine(Pens.Black, nodes[globalRoute[i], 0] / ratio, nodes[globalRoute[i], 1] / ratio,
+                    nodes[globalRoute[i + 1], 0] / ratio, nodes[globalRoute[i + 1], 1] / ratio);
+            }
+            tourDiagram.Image = cityImage;
         }
     }
 }
